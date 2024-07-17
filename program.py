627007a0c9961e66c329d0eb51c944c75b6b852e
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-07-17 13:56:31 +0200

    Created Discord Instance

--- program.py
+++ program.py
@@ -1,1 +1,15 @@
-print("hello world!")
+import discord
+
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged as: {self.user}')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+intents = discord.Intents.all()
+
+client = Client(intents=intents)
+client.run('token')
