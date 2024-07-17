8cecd520816fd5c89e6f920180f37a1294e4cb6e
⭐PazurK0TA <piotrek.jelonek@icloud.com>
2024-07-17 20:41:22 +0200

    Replaced API provider

--- api_data.py
+++ api_data.py
@@ -3,11 +3,6 @@ import requests as req, json, read_json
 
 # https://www.weatherapi.com/docs/
 def get_request_data():
-    request = f'/current.json?key={read_json.get_api_key()}q=\'Warsaw\''
-    response = req.get(f'https://api.weatherapi.com/v1{request}')
+    request = f'/weather?lat=51.107883&lon=17.038538&appid={read_json.get_api_key()}'
+    response = req.get(f'https://api.openweathermap.org/data/3.0{request}')
     return response.json()
-
-
-def get_json():
-    return json.load(get_request_data())
-
